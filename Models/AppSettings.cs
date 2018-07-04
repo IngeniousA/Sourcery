f7a3936b33e5faab0c259fdf50e0435481efed83
Sergey Rakhmanov <rpden@yandex.ru>
2018-07-04 22:21:53 +0300

    Sourcery Source Code

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -8,7 +8,7 @@ namespace SourceryWeb.Models
     public static class AppSettings
     {
         public static string Url { get; set; } = "<ADDRESS>{0}";
-        public static string Name { get; set; } = "SourceryBot";
+        public static string Name { get; set; } = "<NAME>";
         public static string Key { get; set; } = "<KEY>";
     }
 }
