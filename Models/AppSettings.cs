57d4f606734bd96f2017169ff8cfc9c471a8a78a
Sergey Rakhmanov <rpden@yandex.ru>
2018-07-04 22:20:22 +0300

    Update AppSettings.cs

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -7,8 +7,8 @@ namespace SourceryWeb.Models
 {
     public static class AppSettings
     {
-        public static string Url { get; set; } = "https://sourceryweb.azurewebsites.net:443/{0}";
+        public static string Url { get; set; } = "<ADDRESS>{0}";
         public static string Name { get; set; } = "SourceryBot";
-        public static string Key { get; set; } = "575657984:AAEysWHYdr-obTLTsLA3jjJ8dYR2Kuf-gmA";
+        public static string Key { get; set; } = "<KEY>";
     }
 }
