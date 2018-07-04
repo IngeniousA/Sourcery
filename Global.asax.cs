19a095b741d7b7c5eef08d5af3c325d25387d9ba
Sergey Rakhmanov <rpden@yandex.ru>
2018-07-04 13:07:59 +0300

    VS2017 stuff

---
+++ Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using SourceryWeb.Models;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SourceryWeb
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected async Task AsyncStart()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            await Bot.Get();
+        }
+        protected void Application_Start() => AsyncStart().GetAwaiter().GetResult();
+    }
+}
