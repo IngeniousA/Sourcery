cfeebcf5d33838787c866e2d96804900315e1924
Sergey Rakhmanov <rpden@yandex.ru>
2018-07-04 13:04:44 +0300

    Controllers

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SourceryWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return @"  ______                                                                        
+ /      \                                                                       
+/$$$$$$  |  ______   __    __   ______    _______   ______    ______   __    __ 
+$$ \__$$/  /      \ /  |  /  | /      \  /       | /      \  /      \ /  |  /  |
+$$      \ /$$$$$$  |$$ |  $$ |/$$$$$$  |/$$$$$$$/ /$$$$$$  |/$$$$$$  |$$ |  $$ |
+ $$$$$$  |$$ |  $$ |$$ |  $$ |$$ |  $$/ $$ |      $$    $$ |$$ |  $$/ $$ |  $$ |
+/  \__$$ |$$ \__$$ |$$ \__$$ |$$ |      $$ \_____ $$$$$$$$/ $$ |      $$ \__$$ |
+$$    $$/ $$    $$/ $$    $$/ $$ |      $$       |$$       |$$ |      $$    $$ |
+ $$$$$$/   $$$$$$/   $$$$$$/  $$/        $$$$$$$/  $$$$$$$/ $$/        $$$$$$$ |
+                                                                      /  \__$$ |
+                                                                      $$    $$/ 
+                                                                       $$$$$$/  ";
+        }
+    }
+}
